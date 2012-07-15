89ed890110864831b80cbe18d01137f9bb01adc2
Sami <dev@01.fi>
2012-07-15 19:28:22 +0300

    Finetuned apt_conf module

--- modules/apt_conf/manifests/init.pp
+++ modules/apt_conf/manifests/init.pp
@@ -2,12 +2,11 @@ class apt_conf {
 	$file = '/etc/apt/sources.list'
 
 	file { $file:
-		content => template('apt_conf/etc/apt/sources.list.erb');
+		content => template('apt_conf/etc/apt/sources.list.erb'),
 	}
 
 	exec { 'aptitude update':
-		command => '/usr/bin/aptitude update',
 		subscribe   => File[$file],
-		refreshonly => true
+		refreshonly => true,
 	}
 }
