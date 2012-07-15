774ec5a8cf36a99114eaec36cedc070dfc18c711
Sami <hostikka@gmail.com>
2012-07-15 19:35:34 +0300

    Finetuned slim module

--- modules/slim/manifests/init.pp
+++ modules/slim/manifests/init.pp
@@ -3,20 +3,14 @@ class slim {
 
         service { $package:
                 ensure => running,
-                require => Package[$package]
+                require => Package[$package],
         }
 
-        package { $package:
-                ensure => installed
-        }
+        package { $package: }
 
 	file { '/etc/slim.conf':
-		ensure  => file,
 		source  => 'puppet:///modules/slim/etc/slim.conf',
-		owner   => 'root',
-		group   => 'root',
-		mode    => '0444',
-		require => Package[$package]
+		require => Package[$package],
         }
 
 }
