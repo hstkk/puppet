96cdd13633783548b4f8755cb6c8628e8e912a26
Sami <dev@01.fi>
2012-08-29 15:24:07 +0300

    Finetuned puppet module

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -2,7 +2,7 @@ class puppet {
 	$package = 'puppet'
 	package { $package: }
 
-	file { '/etc/puppet.conf':
+	file { '/etc/puppet/puppet.conf':
 		content => template('puppet/etc/puppet/puppet.conf.erb'),
 		require => Package[$package],
 	}
