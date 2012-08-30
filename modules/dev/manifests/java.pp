986e53c9e8d32b2209cd5f3ae825b3cc6c897354
Sami <dev@01.fi>
2012-08-30 20:28:18 +0300

    Added play framework

--- modules/dev/manifests/java.pp
+++ modules/dev/manifests/java.pp
@@ -1,4 +1,23 @@
 class dev::java {
 	$packages = [ 'eclipse-jdt', 'eclipse-pde', 'eclipse' ]
 	package { $packages: }
+
+	$path = '/tmp/repo-deb-build-0002.deb'
+	exec { 'download-typesafe':
+		cwd => '/tmp',
+		command => 'wget http://apt.typesafe.com/repo-deb-build-0002.deb',
+		creates => $path,
+	}
+	package { 'typesafe':
+		ensure => held,
+		provider => dpkg,
+		source   => $path,
+		require  => Exec['download-typesafe'],
+	}
+	exec { 'apt-get update':
+		require  => package['typesafe'],
+	}
+	package { 'typesafe-stack':
+		require  => Exec['apt-get update'],
+	}
 }
