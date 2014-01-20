71abb75d4158fb6ccf17914cc6bc03705e0d751e
hstkk <hostikka@gmail.com>
2014-01-20 02:00:01 +0200

    Fix ssh keys

--- modules/raspberrypi/manifests/sami.pp
+++ modules/raspberrypi/manifests/sami.pp
@@ -13,12 +13,19 @@ class raspberrypi::sami {
 
 	irssi::autostart { 'sami': }
 
-	$users = ['sami', 'git']
-	ssh_authorized_key { 'sami':
+	Ssh_authorized_key {
 		ensure  => present,
-		user    => $users,
 		key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCnhZ7QF5UeQ94eCresZw9OHGtpLRKLYUsQYrgJOQQk3MdVj1h3ofu5F5reFxigjzk+0H60YzjkgCJDg1xK93yaP66JfEweprPfEegc9UAJnWnjU6GbE3NpLwZSU/QoXShK2TunNUbrOUr25U/gCLVe2tdKIU1dDKtHRDrQ6vbzqtKTz+zq0JIFI1fkZzvIyMISH8qFayQbXmgS6m8Zvn/S0oScpRToTgLiqs0rSLtT2sXH5YsJjBC7yenCHo5FclwAw1T6r2NHHfHoSJqSKcCmTCfHgPfl6EfPvDarLVoOeTlY8//PnWh3/uTMq7xfX7Ts5BsDKmBvTCVCX1k91Pcx',
 		type    => 'ssh-rsa',
-		require => User[$users],
+	}
+
+	ssh_authorized_key { 'sami':
+		user => 'sami',
+		require => User['sami'],
+	}
+
+	ssh_authorized_key { 'git':
+		user => 'git',
+		require => User['git'],
 	}
 }
