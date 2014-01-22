5701616f626b6cd93f2a20ba95409328fb9bf54c
hstkk <hostikka@gmail.com>
2014-01-22 23:50:58 +0200

    Fix local backup again

--- modules/raspberrypi/manifests/backup.pp
+++ modules/raspberrypi/manifests/backup.pp
@@ -6,11 +6,15 @@ define raspberrypi::backup (
 		file { $dest:
 			ensure => 'directory',
 		}
-	}
+	} ->
+
+	if ! defined(Package['rsync']) {
+		package['rsync']
+	} ->
 
 	cron { "backup-${src}":
 		command => "rsync --archive --delete ${src} ${dest} &> /dev/null",
 		special => 'daily',
-		require => [Package['rsync'], File[$src, $dest]],
+		require => File[$src],
 	}
 }
